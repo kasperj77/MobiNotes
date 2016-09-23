8660c242b2affc53f95cf73f4dcd4efee64e0ba6
Jordan Kasper <jordan.kasper@gmail.com>
2016-09-23 14:42:01 -0700

    Creating Note class and defining member variables along with getter and setter functions

---
+++ app/src/main/java/com/mobigolabs/mobinotes/Note.java
@@ -0,0 +1,54 @@
+package com.mobigolabs.mobinotes;
+
+/**
+ * Created by jordan on 9/23/16.
+ */
+public class Note {
+
+    private String mTitle;
+    private String mDescription;
+    private boolean mIdea;
+    private boolean mToDo;
+    private boolean mImportant;
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String mDescription) {
+        this.mDescription = mDescription;
+    }
+
+    public boolean isIdea() {
+        return mIdea;
+    }
+
+    public void setIdea(boolean mIdea) {
+        this.mIdea = mIdea;
+    }
+
+    public boolean isToDo() {
+        return mToDo;
+    }
+
+    public void setToDo(boolean mToDo) {
+        this.mToDo = mToDo;
+    }
+
+    public boolean isImportant() {
+        return mImportant;
+    }
+
+    public void setImportant(boolean mImportant) {
+        this.mImportant = mImportant;
+    }
+
+}
