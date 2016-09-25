e481488acca71ecf8b44ccdef05280a5b8e6117a
Jordan Kasper <jordan.kasper@gmail.com>
2016-09-24 20:53:38 -0700

    inflating XML layout and implementing create new note

--- app/src/main/java/com/mobigolabs/mobinotes/DialogNewNote.java
+++ app/src/main/java/com/mobigolabs/mobinotes/DialogNewNote.java
@@ -19,18 +19,51 @@ public class DialogNewNote extends DialogFragment {
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View dialogView = inflater.inflate(R.layout.dialog_new_note, null);
 
-        final EditText editTitle = (EditText) dialogView.findViewById
-                (R.id.editTitle);
-        final EditText editDescription = (EditText) dialogView.findViewById
-                (R.id.editDescription);
-        final CheckBox checkBoxIdea = (CheckBox) dialogView.findViewById
-                (R.id.checkBoxIdea);
-        final CheckBox checkBoxTodo = (CheckBox) dialogView.findViewById
-                (R.id.checkBoxTodo);
-        final CheckBox checkBoxImportant = (CheckBox) dialogView.findViewById
-                (R.id.checkBoxImportant);
+        final EditText editTitle = (EditText) dialogView.findViewById(R.id.editTitle);
+        final EditText editDescription = (EditText) dialogView.findViewById(R.id.editDescription);
+        final CheckBox checkBoxIdea = (CheckBox) dialogView.findViewById(R.id.checkBoxIdea);
+        final CheckBox checkBoxTodo = (CheckBox) dialogView.findViewById(R.id.checkBoxTodo);
+        final CheckBox checkBoxImportant = (CheckBox) dialogView.findViewById(R.id.checkBoxImportant);
+
         Button btnCancel = (Button) dialogView.findViewById(R.id.btnCancel);
         Button btnOK = (Button) dialogView.findViewById(R.id.btnOK);
 
+        builder.setView(dialogView).setMessage("Add a new note");
+
+        btnCancel.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        // Handle the OK button
+        
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+
+                // Create a new note
+                Note newNote = new Note();
+
+                //Set its variables to match the users entries on the form
+                newNote.setTitle(editTitle.getText().toString());
+                newNote.setDescription(editDescription.getText().toString());
+                newNote.setIdea(checkBoxIdea.isChecked());
+                newNote.setToDo(checkBoxTodo.isChecked());
+                newNote.setImportant(checkBoxImportant.isChecked());
+
+                // Get a reference to MainActivity
+                MainActivity callingActivity = (MainActivity) getActivity();
+
+                // Pass newNote back to MainActivity
+                callingActivity.createNewNote(newNote);
+
+                //Quit the dialog
+                dismiss();
+            }
+        });
+
+        return builder.create();
     }
 }
